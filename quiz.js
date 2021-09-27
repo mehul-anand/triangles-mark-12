3e0b6a478d9065337d52625276638cd2f2266b0d
mehul-anand <89949898+mehul-anand@users.noreply.github.com>
2021-09-28 03:14:43 +0530

    link html and js

---
+++ quiz.js
@@ -0,0 +1,6 @@
+const quizForm = document.querySelector(".quiz");
+const submitBtn = document.querySelector("#submit");
+const output = document.querySelector("#output");
+
+const answers = ["90°", "right angled"];
+
