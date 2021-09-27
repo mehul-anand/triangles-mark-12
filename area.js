22a06d2c1903e7581ea1ef6365c79a7d12fea9c4
mehul-anand <89949898+mehul-anand@users.noreply.github.com>
2021-09-28 03:19:54 +0530

    feat : added find area button

--- area.js
+++ area.js
@@ -2,3 +2,22 @@ const sides = document.querySelectorAll(".side");
 const areaBtn = document.querySelector("#area-button");
 const output = document.querySelector("#output");
 
+function checkNegatives(){
+    if (Number(sides[0].value)<0 || Number(sides[1].value)<0){
+       return "Negative" 
+    }else{
+        return "Positive"
+    }
+}
+
+function displayArea(){
+    const checker =checkNegatives()
+    if (checker == "Positive"){
+        const area = 0.5 * Number(sides[0].value) *  Number(sides[1].value)
+        output.innerText = "Area is : "+area+" cm²"
+    }else{
+        output.innerText = "Base or Height can't be negative"
+    }
+}
+
+areaBtn.addEventListener("click",displayArea)
