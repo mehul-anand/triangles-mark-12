f6e9a1b5daeaab721e7d2e2cd72b2847ff2966e6
mehul-anand <89949898+mehul-anand@users.noreply.github.com>
2021-09-28 03:17:11 +0530

    feat : added submit button

--- quiz.js
+++ quiz.js
@@ -4,3 +4,16 @@ const output = document.querySelector("#output");
 
 const answers = ["90°", "right angled"];
 
+function sumbitAndCheck() {
+    let score = 0;
+    let index = 0;
+    const formResults = new FormData(quizForm);
+    for (let value of formResults.values()) {
+        if (value === answers[index]) {
+            score = score + 1;
+        }index = index + 1;
+    }   
+    output.innerText = "Your Score is : " + score;
+}
+
+submitBtn.addEventListener("click", sumbitAndCheck)
